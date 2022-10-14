8b7befd534c389218d03f1f4a8e92fd2c6260c24
Davi Batista <davibatista.gb@gmail.com>
2022-10-13 23:36:19 -0300

    aula 02

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -6,17 +6,28 @@ export interface HeadingProps {
   size?: "sm" | "md" | "lg";
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Heading({ size = "md", children, asChild }: HeadingProps) {
+export function Heading({
+  size = "md",
+  children,
+  asChild,
+  className,
+}: HeadingProps) {
   const Comp = asChild ? Slot : "h2";
   return (
     <Comp
-      className={clsx("text-gray-100", "font-sans", {
-        "text-lg": size === "sm",
-        "text-xl": size === "md",
-        "text-xxl": size === "lg",
-      })}
+      className={clsx(
+        "text-gray-100",
+        "font-sans",
+        {
+          "text-lg": size === "sm",
+          "text-xl": size === "md",
+          "text-xxl": size === "lg",
+        },
+        className
+      )}
     >
       {children}
     </Comp>

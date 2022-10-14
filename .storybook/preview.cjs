1ce616e45869ceb1225e3cab8ad385a6867c3120
Davi Batista <davibatista.gb@gmail.com>
2022-10-14 14:23:08 -0300

    aula extra

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,5 +1,14 @@
-import '../src/styles/global.css';
 import { themes } from '@storybook/theming';
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+import '../src/styles/global.css';
+
+// MSW
+initialize({
+  onUnhandledRequest: 'bypass'
+});
+export const decorators = [mswDecorator];
+
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
